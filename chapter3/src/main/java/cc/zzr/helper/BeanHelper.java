15f4be87378fd420ab4fb5ecec5a33b88bb056b4
zhaoyancong <chancong521@qq.com>
2018-01-20 14:56:55 +0800

    add utils and helper

---
+++ chapter3/src/main/java/cc/zzr/helper/BeanHelper.java
@@ -0,0 +1,49 @@
+package cc.zzr.helper;
+
+import cc.zzr.util.ReflectionUtil;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * bean助手类
+ */
+public class BeanHelper {
+    private static final Map<Class<?>, Object> BEAN_MAP = new HashMap();
+
+    static {
+        Set<Class<?>> beanClassSet = ClassHelper.getBeanClassSet();
+        for (Class<?> beanClass : beanClassSet) {
+            Object obj = ReflectionUtil.getInstance(beanClass);
+            BEAN_MAP.put(beanClass, obj);
+        }
+    }
+
+    /**
+     * 获取bean映射
+     * @return
+     */
+    public static Map<Class<?>, Object> getBeanMap() {
+        return BEAN_MAP;
+    }
+
+    /**
+     * 获取bean实例
+     * @param cls
+     * @param <T>
+     * @return
+     */
+    public static <T> T getBean(Class<T> cls) {
+        if (!BEAN_MAP.containsKey(cls)) {
+            throw new RuntimeException("can not get bean by class :" + cls);
+        }
+        return (T)BEAN_MAP.get(cls);
+    }
+
+    public static void setBean(Class<?> cls, Object obj) {
+        BEAN_MAP.put(cls, obj);
+    }
+
+
+}
